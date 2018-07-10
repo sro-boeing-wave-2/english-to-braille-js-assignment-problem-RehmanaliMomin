34c6f6b5533b729a2d09d1081756465876f7db2f
rehman <rehmanmomin@gmail.com>
2018-07-10 16:50:09 +0530

    changes kara

--- index.js
+++ index.js
@@ -7,39 +7,30 @@ import englishToBrailleLiteralSet from './english-to-braille.js';
 * in english language to that in the equivalent braille
 * code.
 */
-var btn;
-var txt;
-var i;
-var ans = '';
-var temp;
-var res;
-var myMap = new Map(englishToBrailleLiteralSet);
+let txt;
+let i;
+let ans = '';
+let temp;
+let res;
+const myMap = new Map(englishToBrailleLiteralSet);
 // var imported = document.createElement('english-to-braille');
 // imported.src = '/english-to-braille.js';
 // document.head.appendChild(imported);
 
 
-window.onload = function () {
-
-  btn = document.getElementById('btnConvertEnglishToBraille');
-  btn.addEventListener('click', result);
-
-}
-
-function result(e) {
-
-  txt = document.getElementById('sourceLangText').value;
-  res = formulate(txt);
-  document.getElementById('targetLangText').innerHTML=res;
-
-}
-
 function formulate(text) {
-
-  for (i = 0; i < text.length; i++) {
+  for (i = 0; i < text.length; i += 1) {
     temp = myMap.get(text[i]);
     ans += temp;
   }
   return ans;
 }
 
+function result() {
+  txt = document.getElementById('sourceLangText').value;
+  res = formulate(txt);
+  document.getElementById('targetLangText').innerHTML = res;
+}
+
+const btnn = document.getElementById('btnConvertEnglishToBraille');
+btnn.addEventListener('click', result);
