606f35a1e52fef90dc8899e7c21618f39de75a11
rehman <rehmanmomin@gmail.com>
2018-07-10 16:22:25 +0530

    committed first time

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
 /**
- * Converts text in english to braille code.
- * Write all code related to the DOM Manipulation here.
- * Using the english-to-braille mapper convert the text
- * in english language to that in the equivalent braille
- * code.
- */
+* Converts text in english to braille code.
+* Write all code related to the DOM Manipulation here.
+* Using the english-to-braille mapper convert the text
+* in english language to that in the equivalent braille
+* code.
+*/
+var btn;
+var txt;
+var i;
+var ans = '';
+var temp;
+var res;
+var myMap = new Map(englishToBrailleLiteralSet);
+// var imported = document.createElement('english-to-braille');
+// imported.src = '/english-to-braille.js';
+// document.head.appendChild(imported);
+
+
+window.onload = function () {
+
+  btn = document.getElementById('btnConvertEnglishToBraille');
+  btn.addEventListener('click', result);
+
+}
+
+function result(e) {
+
+  txt = document.getElementById('sourceLangText').value;
+  res = formulate(txt);
+  document.getElementById('targetLangText').innerHTML=res;
+
+}
+
+function formulate(text) {
+
+  for (i = 0; i < text.length; i++) {
+    temp = myMap.get(text[i]);
+    ans += temp;
+  }
+  return ans;
+}
+
